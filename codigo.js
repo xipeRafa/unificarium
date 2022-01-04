2247699ee03d2a79faad6fdcebc823a9bdf7716e
rafael <rafaelsistem87@gmail.com>
2022-01-04 16:04:53 -0700

    f

---
+++ codigo.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+window.onload = function(){
+      app('https:xiperafa.github.io/taxistas')
+}
+
+
+ const app = (data) =>{
+
+
+   
+           /*  var link = "https://smartwateracademy.com/video/mivideo.php" */
+     /*        var iframe = document.createElement('iframe');  */
+          /*   let iframe = document.getElementById('') */
+          /*   iframe.frameBorder=0;
+            iframe.width="100%";
+            iframe.height="800px";
+            /* iframe.style="position: absolute; left: 13%; margin-top: 33px;"; */
+           /*  iframe.id="randomid";
+            iframe */ 
+            document.getElementById("app").setAttribute("src", data);
+         
+         
+      }
+ 
+
